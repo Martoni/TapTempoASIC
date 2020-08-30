808ac1a516f0fbdfc74c1571ee36c0981fa305e6
Fabien Marteau <fabien.marteau@armadeus.com>
2020-08-30 08:33:29 +0200

    percount: (im)prove module

--- hdl/percount.v
+++ hdl/percount.v
@@ -33,20 +33,23 @@ begin
 end
 
 reg [BPM_REG_SIZE:0] counter = 0;
+reg counter_valid = 0;
 
-reg btn_old = 1'b0;
-wire btn_rise = !btn_old & btn_i;
+assign btn_per_valid = counter_valid;
+assign btn_per_o = counter;
+
+reg btn_old;
+wire btn_rise = (!btn_old) & btn_i;
 
 always@(posedge clk_i or posedge rst_i)
 begin
     if(rst_i)
-        btn_old <= 0;
+        btn_old <= btn_i;
     else
     begin
         btn_old <= btn_i;     
     end
 end
-
 
 always@(posedge clk_i or posedge rst_i)
 begin
@@ -57,16 +60,16 @@ begin
         if(btn_rise) 
         begin
             counter <= 0;
-            btn_per_valid <= 1'b1;
+            counter_valid <= 1'b1;
         end else 
         begin
-            btn_per_valid <= 1'b0;
-            if(tp_i)
+            counter_valid <= 1'b0;
+            /* stop counting if max, count tp_i */
+            if(tp_i && counter < `BPM_PER_MAX)
                 counter <= counter + 1'b1;
         end
     end
 end
-
 
 /*********************/
 /* Yosys formal part */
@@ -77,15 +80,39 @@ end
 
     initial begin
         past_valid <= 1'b0;
+        /* push reset at begin */
+        assume(rst_i);
     end
 
 always @(posedge clk_i) begin
 	past_valid <= 1'b1;
 
+    /* tp_i is one cycle length */
+    if(tp_i && !rst_i && past_valid)
+        assume(!$past(tp_i));
+
+    /* btn_rise is one cycle length */
+    if(btn_rise && !rst_i && past_valid)
+        assert(!$past(btn_rise));
+
+    /* When tp_i==1, counter increase if btn_o not rising and counter less
+    * than `BPM_PER_MAX*/
+    if(past_valid && !rst_i && $past(tp_i) &&
+        !$past(btn_rise) && counter!=0 && (counter < `BPM_PER_MAX))
+        assert(counter == $past(counter) + 1'b1);
+
+    /* when btn_i rose, counter is reset and valid is 1 */
+    if(past_valid && !rst_i && $past(btn_rise))
+    begin
+        assert(counter == 1'b0);
+        assert(counter_valid == 1'b1);
+    end
+
+    if(past_valid)
+        assert(counter <= `BPM_PER_MAX);
+
+    cover(counter_valid);
 end
-
 `endif
-
-
 
 endmodule
