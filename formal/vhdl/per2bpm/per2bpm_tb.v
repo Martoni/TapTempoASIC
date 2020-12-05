6f6ccb2e58bdd212e3aa13f9e6f3a0583c638b93
Fabien Marteau <fabien.marteau@armadeus.com>
2020-12-05 08:22:52 +0100

    vhdl/formal/per2bpm: working cover statement

--- formal/vhdl/per2bpm/per2bpm_tb.v
+++ formal/vhdl/per2bpm/per2bpm_tb.v
@@ -1,5 +1,12 @@
 module per2bpm_tb;
 
+    wire clk_i;
+    wire rst_i;
+    wire [23:0] btn_per_i;
+    wire btn_per_valid;
+    wire [7:0] bpm_o;
+    wire bpm_valid;
+
     per2bpm DUT (
         .clk_i (clk_i),
         .rst_i (rst_i),
@@ -12,14 +19,18 @@ module per2bpm_tb;
     reg past_valid;
     
     initial begin
+        assume(btn_per_i == 0);
+        assume(btn_per_valid == 0);
         past_valid <= 1'b0;
         assume(rst_i);
     end
 
     always @(posedge clk_i)
     begin
+        cover(bpm_valid == 1);
         if(rst_i)
             assert(bpm_o == 1'b0);
     end
 
+    
 endmodule
